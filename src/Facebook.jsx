1369c9829a81df5a9cecc2156cfd7d8e9771b8b0
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-10-10 12:36:09 +0200

    solve iteration 13

--- src/Facebook.jsx
+++ src/Facebook.jsx
@@ -1,7 +1,35 @@
 import profiles from './data/berlin.json';
 
 const Facebook = () => {
-  return <div></div>;
+  return (
+    <ul class="usersList">
+      {profiles.map((profile) => {
+        return (
+          <li key={profile.img} className="profile IdCard">
+            <img src={profile.img} alt={profile.lastName} />
+            <div className="info">
+              <div className="field">
+                <strong>First name: </strong>
+                <span>{profile.firstName}</span>
+              </div>
+              <div className="field">
+                <strong>Last name: </strong>
+                <span>{profile.lastName}</span>
+              </div>
+              <div className="field">
+                <strong>Country: </strong>
+                <span>{profile.country}</span>
+              </div>
+              <div className="field">
+                <strong>Type: </strong>
+                <span>{(profile.isStudent && 'Student') || 'Teacher'}</span>
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Facebook;
